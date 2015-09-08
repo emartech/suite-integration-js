b8110f63512f77495a78631ddd11696ae45aae06
József Kozma <jozsef.kozma@gmail.com>
2015-09-08 13:26:14 +0200

    fixing the test too

--- client/app/message_handler/resize.spec.js
+++ client/app/message_handler/resize.spec.js
@@ -19,7 +19,7 @@ describe('Resize Handler', function() {
     messageHandler.getIntegrationIframe({
       integration_instance_id: integrationInstanceId
     });
-    expect(fakeWindow.document.getElementById).to.have.been.calledWith('#integration-' + integrationInstanceId);
+    expect(fakeWindow.document.getElementById).to.have.been.calledWith('integration-' + integrationInstanceId);
   });
 
   it('should return element when calling getIntegrationIframe with valid source', function() {
