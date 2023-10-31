338ee9b7160a9d8da3e02e8a5ad7924227835013
rudywaltz <david.barkoczi@gmail.com>
2023-10-31 13:45:58 +0100

    feat(logger): add debug_data
    EME-8620
    
    co-authored-by: Marton Visnovitz <marton.visnovitz@emarsys.com>

--- client/app/logger/index.js
+++ client/app/logger/index.js
@@ -6,16 +6,33 @@ class Logger {
       return;
     };
 
-    window.e.utils.sendLog({
-      action,
-      level: 30,
-      'service-name': 'suite-integration-js',
-      name: 'suite-integration-js',
-      user: Logger._getUserData(),
-      time: (new Date()).toISOString(),
-      referrer: Logger._getReferrer(),
-      page_load_id: String(Math.random())
+    window.e.utils.getTopUrl().then(url => {
+      const list = {};
+      const topFrameUrl = new URL(url);
+      const searchParams = new URLSearchParams(topFrameUrl.search);
+
+      ['action', 'r', 'service', 'service_product', 'show', 'step', 'from_action'].forEach(key => {
+        const value = searchParams.get(key);
+
+        if (value) {
+          list[key] = JSON.stringify(value);
+        }
+      });
+
+      window.e.utils.sendLog({
+        action,
+        level: 30,
+        'service-name': 'suite-integration-js',
+        name: 'suite-integration-js',
+        user: Logger._getUserData(),
+        time: (new Date()).toISOString(),
+        referrer: Logger._getReferrer(),
+        page_load_id: String(Math.random()),
+        debug_data: JSON.stringify(list)
+      });
     });
+
+
   }
   static _getReferrer() {
     return [window.document.referrer, window.document.location.href]
